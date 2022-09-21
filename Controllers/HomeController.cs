71f82378e96d2750c9a4628ab53a52d3045b4b02
Supriya K J <86228362+321supriya@users.noreply.github.com>
2022-09-21 13:08:12 +0530

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UserClass;
+using UserClass.Model;
+
+namespace InsuranceProjectMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+
+        public ActionResult Index()
+        {
+            if (Session["CurrentUser"] != null)
+            {
+                 
+                var userDetails = Session["CurrentUser"] as UserTable;
+                return "<h1>HOME PAGE</h1><hr/>Welcome to User: " + userDetails.userFirstName;
+            }
+            else
+                return "<h1>HOME PAGE</h1><hr/>Welcome to Anonymous User";
+        }
+        public ActionResult UpdateUser()
+        {
+            UserTable model = new UserTable();
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult UpdateUser(UserTable updateUser)
+        {
+            var context = new InsuranceDBEntities();
+            IUserModule com = new UserComponent();
+
+            var rec = context.UserTables.FirstOrDefault((e) => e.userId == updateUser.userId);
+
+            try
+            {
+                com.UpdateUserDetails(rec.userAddress, rec.userContact.ToString(), rec.userId);
+                return RedirectToAction("UpdateUser");
+            }
+            catch (CustomerAlreadyExistsException ex)
+            {
+                ModelState.AddModelError("CustomerError", ex.Message);
+                return View(updateUser);
+            }
+        }
+
+    }
+}
---
+++ Controllers/UserController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using UserClass;
+using UserClass.Model;
+
+namespace InsuranceProjectMVC.Controllers
+{
+    [AllowAnonymous]
+    public class UserController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string email, string password)
+        {
+            var com = new UserComponent();
+            var user = com.ValidateUser(email, password);
+            if (user == null)
+            {
+                ModelState.AddModelError("LoginFailure", "Login failed for the user");
+                return View();
+            }
+            Session["CurrentUser"] = user;
+            FormsAuthentication.SetAuthCookie(user.userEmail, false);
+            FormsAuthentication.RedirectFromLoginPage(user.userEmail, false);
+            return RedirectToAction("Index", "Home");
+        }
+        public ActionResult RegisterNew()
+        {
+            UserTable model = new UserTable();
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult RegisterNew(UserTable postedData)
+        {
+            IUserModule com = new UserComponent();
+            try
+            {
+                com.RegisterUser(postedData);
+                return RedirectToAction("Login");
+            }
+            catch (CustomerAlreadyExistsException ex)
+            {
+                ModelState.AddModelError("CustomerError", ex.Message);
+                return View(postedData);
+            }
+        }
+
+        
+
+
+    }
+}
