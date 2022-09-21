bfb37682bb2c8690158efbb2a652956b3bfcae36
Ragini340 <88105537+Ragini340@users.noreply.github.com>
2022-09-21 16:52:46 +0530

    Add files via upload

---
+++ PolicyOrder.aspx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace InsuranceProject.User
+{
+	public partial class PolicyOrder : System.Web.UI.Page
+	{
+
+         protected void Page_Load(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+
+
+        protected void ddlpid_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+
+
+        protected void btnadd_Click(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+
+
+        protected void btnCancel_Click(object sender, EventArgs e)
+        {
+            
+
+
+        }
+
+
+
+        protected void txtamount_TextChanged(object sender, EventArgs e)
+        {
+
+
+
+        }
+    }
+}
